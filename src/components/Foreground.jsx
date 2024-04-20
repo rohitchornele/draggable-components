442a19b2bcb1a4e8997d3fe37c5c0a89de652353
Rohit Chornele <rohitchornele@gmail.com>
2024-04-20 23:48:54 +0530

    Added some more component data

--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -10,14 +10,26 @@ function Foreground() {
             desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, qui.",
             filesize: "0.9 MB",
             close: false,
-            tag: { isOpen: true, tagTitle: "Upload Now", tagColor: "blue"}
+            tag: { isOpen: true, tagTitle: "Try Dragging Me", tagColor: "blue"}
         },
         {
             desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-            filesize: "0.9 MB",
+            filesize: "0.7 MB",
             close: false,
-            tag: { isOpen: true, tagTitle: "Download Now", tagColor: "red"}
-        }
+            tag: { isOpen: true, tagTitle: "Drag me too!!", tagColor: "green"}
+        },
+        {
+          desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ipsum dolor sit amet",
+          filesize: "1.2 MB",
+          close: false,
+          tag: { isOpen: false, tagTitle: "Hey!! Drag me", tagColor: "green"}
+      },
+      {
+        desc: "Lorem ipsum dolor sit . ipsum dolor sit amet",
+        filesize: "0.5 MB",
+        close: false,
+        tag: { isOpen: true, tagTitle: "Drag me again..", tagColor: "blue"}
+    }
     ];
   return (
     <>
